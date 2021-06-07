3d69d14c98207e564882e6877a54c1ff693f7d76
Bai Shui <pstereom@gmail.com>
2021-06-07 19:37:31 +0800

    feat:update

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -3,12 +3,10 @@ import './index.css';
 import { Button } from 'antd';
 import img from '../static/images/buka.png';
 
-const Home = () => {
-  return (
+const Home = () => (
     <div className="testTt">
       <img src={img} />
       <Button type="ghost">reffef</Button>
     </div>
   );
-};
 export default Home;
